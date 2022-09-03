4c8c4d735da33c5350e24feb8cb17113c9c04a34
NahyeonKim <parakise13@gmail.com>
2022-09-03 23:29:30 +0900

    modify BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
