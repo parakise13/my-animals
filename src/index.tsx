c1097cfde77ddf2224b04430af1a81fde692febc
NahyeonKim <parakise13@gmail.com>
2022-09-04 11:20:07 +0900

    modify router

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
     {/* </BrowserRouter> */}
